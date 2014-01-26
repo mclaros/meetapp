ebca6156a34c80c5affebd853915ab357e759537
Mainor <m_claros@live.com>
2014-01-25 19:01:09 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meetapp::Application.config.secret_token = 'c19610e3589481bc88c40965220aac1654b9455d19887e7d316db54f9d6c227093f3ee7efaf477dda0e68ccd0423006936a41f601eb88000c9c42a0e38622504'
