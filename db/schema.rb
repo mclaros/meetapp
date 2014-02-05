c4fda50fe09ea8d5a84c9bab7ccbc7f5531c6cef
Mainor <m_claros@live.com>
2014-02-04 19:42:28 -0800

    add null, unique restrictions to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140201033354) do
+ActiveRecord::Schema.define(:version => 20140205033856) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -31,10 +31,11 @@ ActiveRecord::Schema.define(:version => 20140201033354) do
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
     t.string   "password_digest",                :null => false
-    t.string   "session_token"
+    t.string   "session_token",                  :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["session_token"], :name => "index_users_on_session_token", :unique => true
   add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
