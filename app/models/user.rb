1628fd111bc249eed0b29e0cca03427209e8db67
Mainor <m_claros@live.com>
2014-02-04 18:36:33 -0800

    add password, password_digest modifiers, validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ActiveRecord::Base
-  attr_accessible :bio, :date_of_birth, :email, :full_name, :location, :username
+  attr_accessible :bio, :date_of_birth, :email, :full_name, :location, :username, :password
+  attr_reader :password
+
   validates_presence_of :username, :full_name, :email
-  validates_uniquness_of :username, :email
+  validates_presence_of :password_digest, :message => "Password can't be blank"
+  validates_uniqueness_of :username, :email
   validates_length_of :username, in: 2..15
   validates_length_of :bio, maximum: 500
+  validates_length_of :password, minimum: 6, allow_nil: true
+
+  def password=(password)
+  	@password = password
+  	self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+  	BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
