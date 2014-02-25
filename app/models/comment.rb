93c151fe9ca0b5350d83c9903d89d62e97315278
Mainor <m_claros@live.com>
2014-02-25 13:36:32 -0800

    fix type on Comment polymorphic belongs_to; update erd

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   attr_accessible :author_id, :commentable_id, :commentable_type
 
-  belongs_to :commentable, :polyhmorphic => true
+  belongs_to :commentable, :polymorphic => true
   belongs_to :author,
   					:class_name => "User",
   					:foreign_key => :author_id
