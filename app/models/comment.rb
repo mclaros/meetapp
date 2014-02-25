73dabcc9b76efa4f630ac7d7d0a003525d1d00b7
Mainor <m_claros@live.com>
2014-02-25 15:02:28 -0800

    add comments_list and comments_form partials

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,6 @@ class Comment < ActiveRecord::Base
   belongs_to :author,
   					:class_name => "User",
   					:foreign_key => :author_id
+  has_many :replies, :as => :commentable,
+  				:class_name => "Comment"
 end
